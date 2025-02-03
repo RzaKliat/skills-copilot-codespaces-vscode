9e5e06e62b52ccdbb233c1fcf1c43a21c5bb3d68
rezakeliat <102411817+RzaKliat@users.noreply.github.com>
2025-02-03 13:57:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,31 @@
-// create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server 
+// Create a web server that listens on port 3000
+// The server should respond to a GET request to /comments with a JSON object that looks like this:
+//  {
+//    "comments": [
+//      { "username": "Todd", "comment": "lol" },
+//      { "username": "Derek", "comment": "rofl" }
+//    ]
+//  }
 
-// get method
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/comments' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      comments: [
+        { username: 'Todd', comment: 'lol' },
+        { username: 'Derek', comment: 'rofl' }
+      ]
+    }));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+server.listen(3000);
 
-// post method
-app.post('/', (req, res) => {
-    res.send('Got a POST request');
-});
-
-// put method
-app.put('/user', (req, res) => {
-    res.send('Got a PUT request at /user');
-});
-
-// delete method
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user');
-});
+console.log('Server listening on port 3000');
+// Run node comments.js and visit http://localhost:3000/comments in your browser to see the JSON response.
